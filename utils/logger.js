b137026648cb6d02f73db175a23ee34e757f2eb5
Denis Bezdezhsky <denisb305@gmail.com>
2024-02-12 23:24:34 +0200

    fix logger

--- utils/logger.js
+++ utils/logger.js
@@ -2,12 +2,16 @@ const fs = require("fs");
 const path = require("path");
 const { errorLog } = require("../utils/chalk.log");
 
-const logger = function (req, res, next) {
+const logger = async function (req, res, next) {
+
+  await fileSystemCheck()
+
   const originalSend = res.send;
   res.send = function (data) {
     if (res.statusCode >= 400) {
       const currentDate = new Date().toISOString().split("T")[0];
-      const logFilename = path.join(__dirname, "../logs", `${currentDate}.log`);
+      const logFilename = path.join(__dirname, "/../logs", `${currentDate}.log`);
+
       const logEntry = `Date: ${new Date().toISOString()}, Request: ${
         req.method
       } - ${req.originalUrl}, Status Code: ${
@@ -27,4 +31,10 @@ const logger = function (req, res, next) {
   next();
 };
 
+function fileSystemCheck(){
+  if (!fs.existsSync(`${__dirname}/../logs`)) {
+    console.log("check")
+    fs.mkdirSync(`${__dirname}/../logs`);
+  }
+}
 module.exports = { logger };
